b83ee6738b6388c05b484b440d6831f6442c6cb7
Pedro Ivo <pedroivolima0@gmail.com>
2022-08-25 15:48:46 +1000

    Update ubuSetup.sh

--- ubuSetup.sh
+++ ubuSetup.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # Install
-myInstalls1=("wireshark" "firefox" "dropbox" "google-chrome" "visual-studio-code" "obsidian" "python3" "dotenet")
+myInstalls1=("firefox" "dropbox" "google-chrome" "visual-studio-code" "obsidian" "python3" "dotenet")
 
 for str in ${myInstalls1[@]}; do
     sudo apt install -y $str >>installOutput.txt
