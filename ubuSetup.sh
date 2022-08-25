0665f35a38d057ebf774351fe62b4ba4828274a7
Pedro Ivo <pedroivolima0@gmail.com>
2022-08-25 15:59:12 +1000

    Update ubuSetup.sh

--- ubuSetup.sh
+++ ubuSetup.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # Install
-myInstalls1=("firefox" "dropbox" "google-chrome" "visual-studio-code" "obsidian" "python3" "dotenet")
+myInstalls1=("python3" "dotenet")
 
 for str in ${myInstalls1[@]}; do
     sudo apt install -y $str >>installOutput.txt
